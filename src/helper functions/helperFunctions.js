58715d64fcea0845ce1f6f63afbc3474e88fcacf
Tabbatha Crouch <tabbathacrouch@gmail.com>
2021-08-25 12:42:36 -0500

    add symbols to clean string function

--- src/helper functions/helperFunctions.js
+++ src/helper functions/helperFunctions.js
@@ -1,9 +1,19 @@
 export const cleanString = (string) => {
-  return string
-    .replace(/&quot;/g, '"')
-    .replace(/&#039;/g, "'")
-    .replace(/&rsquo;/g, "'")
-    .replace(/&amp;/g, "&");
+  return (
+    string
+      .replace(/&quot;/g, '"')
+      .replace(/&#039;/g, "'")
+      .replace(/&rsquo;/g, "'")
+      .replace(/&amp;/g, "&")
+      .replace(/&pi;/g, "π ")
+      .replace(/&ldquo;/g, `"`)
+      .replace(/&rdquo;/g, `"`)
+      .replace(/&Eacute;/g, "É")
+      .replace(/&ouml;/g, "ö")
+      // not working in geography category *****fix*****
+      .replace(/&amp;Nu;/g, "Ν")
+      .replace(/&amp;uuml;/g, "ü")
+  );
 };
 
 export const shuffleArray = (array) => {
